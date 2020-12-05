c47903aea9826526917fb2e41a2c1fe02f5d535d
Yovan Naumovski <jovan3.14@gmail.com>
2020-12-05 23:41:55 +0200

    day 4 part 2

--- src/advent2020/day4.clj
+++ src/advent2020/day4.clj
@@ -2,8 +2,21 @@
   (:require [clojure.string :as str]
             [advent2020.input :as input]))
 
+(defn val-bw [str lower upper] (<= lower (Integer/parseInt str) upper))
+
 (def input (input/input-lines "day4"))
+
 (def required ["byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"])
+(def validators {"byr" #(val-bw % 1920 2002)
+                 "iyr" #(val-bw % 2010 2020)
+                 "eyr" #(val-bw % 2020 2030)
+                 "hgt" (fn [i] (let [[_ val unit] (re-matches #"(\d+)(cm|in)" i)]
+                                 (cond (= unit "in") (val-bw val 59 76)
+                                       (= unit "cm") (val-bw val 150 193))))
+                 "hcl" #(re-matches #"#([0-9a-f]{6})" %)
+                 "ecl" #(re-matches #"(amb|blu|brn|gry|grn|hzl|oth)" %)
+                 "pid" #(re-matches #"[0-9]{9}" %)
+                 "cid" (fn [_] true)})
 
 (defn valid? [pass-data]
   (every? #(some (fn [entry] (str/includes? entry %)) pass-data) required))
@@ -15,3 +28,10 @@
 
 (def day4-1 (count (filter valid? pass-data)))
 
+(defn validate-passport-fields [pass-data]
+  (->> pass-data
+       (map #(str/split % #":"))
+       (map (fn [[field val]] ((validators field) val)))
+       (every? identity)))
+
+(def day4-2 (count (filter true? (map validate-passport-fields (filter valid? pass-data)))))
